5d8f3300e5c81f35f32df0618c0a5831156ef1d9
nkgrz <is911n@icloud.com>
2024-10-03 17:19:36 +0300

    Added deleteAll method to clean up DB

--- service/src/test/java/ru/buynest/product/helper/dao/TestCategoryDao.java
+++ service/src/test/java/ru/buynest/product/helper/dao/TestCategoryDao.java
@@ -6,16 +6,26 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.buynest.product.dao.CategoryDao;
 import ru.buynest.product.model.Category;
 
+import java.util.HashMap;
+
 @Repository
 public class TestCategoryDao extends CategoryDao {
 
-    public TestCategoryDao(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        super(namedParameterJdbcTemplate);
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    public TestCategoryDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate);
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     @Transactional
     public int upsert(Category category) {
         return super.upsert(category);
+    }
+
+    @Transactional
+    public void deleteAll() {
+        jdbcTemplate.update("DELETE FROM category", new HashMap<>());
     }
 }
