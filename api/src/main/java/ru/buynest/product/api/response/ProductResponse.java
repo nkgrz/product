d5375b59310e3ce7937645d6d388b3204745f782
nkgrz <is911n@icloud.com>
2024-10-03 17:16:13 +0300

    Added ProductResponse entity

---
+++ api/src/main/java/ru/buynest/product/api/response/ProductResponse.java
@@ -0,0 +1,127 @@
+package ru.buynest.product.api.response;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class ProductResponse {
+    private UUID id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private List<CategoryResponse> categories;
+    private Integer stock;
+    private List<String> images;
+    private Map<String, String> specifications;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private boolean isActive;
+
+    public ProductResponse() {}
+
+    public ProductResponse(UUID id, String name, String description,
+                           BigDecimal price, Integer stock, List<String> images,
+                           Map<String, String> specifications,
+                           Instant createdAt, Instant updatedAt, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.images = images;
+        this.specifications = specifications;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.isActive = isActive;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<CategoryResponse> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<CategoryResponse> categories) {
+        this.categories = categories;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public Map<String, String> getSpecifications() {
+        return specifications;
+    }
+
+    public void setSpecifications(Map<String, String> specifications) {
+        this.specifications = specifications;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
